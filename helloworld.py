e4d7591c05b7a069585f009e65405ab89c2fa018
348845777 <143564252+348845777@users.noreply.github.com>
2023-09-07 18:20:20 -0400

    Create helloworld.py
    Creating first draft

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+from pygame.locals import *
+pygame.init()
+window=pygame.display.set_mode((800, 500)) # Create Screen (width x height)
+window.fill((255, 0, 0))
+pygame.draw.rect(window, (0, 0, 255),
+                 [100, 100, 400, 100], 2)
+run = True
+while run is True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
+
