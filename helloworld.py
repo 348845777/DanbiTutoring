8e2c40f574a523dee7770df36b4b02a40d94d198
348845777 <143564252+348845777@users.noreply.github.com>
2023-09-12 19:06:30 -0400

    Update helloworld.py
    modifications

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,40 @@
 import pygame
-
-
+from screeninfo import get_monitors
+import random
 from pygame.locals import *
 pygame.init()
-window=pygame.display.set_mode((800, 500)) # Create Screen (width x height)
-window.fill((255, 0, 0))
+window=pygame.display.set_mode() # Create Screen (width x height)
+window.fill((0, 0, 0))
+
+squareDimension = 100
+
+monitors = get_monitors() 
+randx = random.randint(0, monitors[0].width - squareDimension)
+
+randy = random.randint(0, monitors[0].height - squareDimension)
+
+
+
 rect = pygame.draw.rect(window, (0, 0, 210),
-                 [100, 100, 400, 100], 2)
+                 [randx, randy, squareDimension, squareDimension], 2)
+
 run = True
 
+print()
 while run is True:
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             click = rect.collidepoint(pygame.mouse.get_pos())
             if click == 1:
-                run = False
+                window.fill((0, 0, 0))
+                randx = random.randint(0, monitors[0].width - squareDimension)
+
+                randy = random.randint(0, monitors[0].height - squareDimension)
+
+                rect = pygame.draw.rect(window, (0, 0, 210), [randx, randy, squareDimension, squareDimension], 2)
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            run = False
         if event.type == pygame.QUIT:
             run = False
     pygame.display.update()
